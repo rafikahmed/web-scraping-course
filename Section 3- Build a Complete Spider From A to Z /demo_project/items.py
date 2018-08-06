5feba10c9cd2adda5efe82dc1a5431d7a3b06494
rafikahmed <djerahahmedrafik@mail.com>
2018-08-06 09:51:28 +0100

    add input & output processors

--- Section 3- Build a Complete Spider From A to Z /demo_project/items.py
+++ Section 3- Build a Complete Spider From A to Z /demo_project/items.py
@@ -6,9 +6,22 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from w3lib.html import remove_tags
 
+def remove_quotations(value):
+    return value.replace(u"\u201d", '').replace(u"\u201c", '')
 
 class QuoteItem(scrapy.Item):
-    text= scrapy.Field()
-    author= scrapy.Field()
-    tags= scrapy.Field()
+    text= scrapy.Field(
+        input_processor= MapCompose(str.strip, remove_quotations),
+        output_processor= TakeFirst()
+    )
+    author= scrapy.Field(
+        input_processor= MapCompose(remove_tags),
+        output_processor= TakeFirst()
+    )
+    tags= scrapy.Field(
+        input_processor= MapCompose(remove_tags),
+        output_processor= Join(',')
+    )
