ad14590e7a54e3d61c68682d14580000c4ec4007
rafikahmed <djerahahmedrafik@mail.com>
2018-08-06 09:46:09 +0100

    add QuoteItem and ItemLoader

--- Section 3- Build a Complete Spider From A to Z /demo_project/items.py
+++ Section 3- Build a Complete Spider From A to Z /demo_project/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class DemoProjectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class QuoteItem(scrapy.Item):
+    text= scrapy.Field()
+    author= scrapy.Field()
+    tags= scrapy.Field()
--- Section 3- Build a Complete Spider From A to Z /demo_project/spiders/goodreads.py
+++ Section 3- Build a Complete Spider From A to Z /demo_project/spiders/goodreads.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from demo_project.items import QuoteItem
 
 
 class GoodReadsSpider(scrapy.Spider):
@@ -14,11 +16,12 @@ class GoodReadsSpider(scrapy.Spider):
     #Response
     def parse(self, response):
         for quote in response.selector.xpath("//div[@class='quote']"):
-            yield {
-                'text': quote.xpath(".//div[@class='quoteText']/text()[1]").extract_first(),
-                'author': quote.xpath(".//div[@class='quoteText']/child::a/text()").extract_first(),
-                'tags': quote.xpath(".//div[@class='greyText smallText left']/a/text()").extract()
-            }
+            loader= ItemLoader(item=QuoteItem(), selector=quote, response=response)
+            loader.add_xpath('text', ".//div[@class='quoteText']/text()[1]")
+            loader.add_xpath('author', ".//div[@class='quoteText']/child::a/text()")
+            loader.add_xpath('tags', ".//div[@class='greyText smallText left']/a/text()")
+            yield loader.load_item()
+            
         
         # /quotes?page=2
         next_page= response.selector.xpath("//a[@class='next_page']/@href").extract_first()
@@ -26,4 +29,3 @@ class GoodReadsSpider(scrapy.Spider):
         if next_page is not None:
             next_page_link= response.urljoin(next_page)
             yield scrapy.Request(url=next_page_link, callback=self.parse)
-
