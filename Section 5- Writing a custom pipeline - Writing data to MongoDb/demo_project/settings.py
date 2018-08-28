2c46eaf2079c0e7c36d4ebcd791f4ffcade65b37
rafikahmed <djerahahmedrafik@mail.com>
2018-08-28 16:19:46 +0100

    add mongodb and mogouri and activating the mongdb pipeline

--- Section 5- Writing a custom pipeline - Writing data to MongoDb/demo_project/settings.py
+++ Section 5- Writing a custom pipeline - Writing data to MongoDb/demo_project/settings.py
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = False
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'demo_project.pipelines.DemoProjectPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'demo_project.pipelines.MongoDbPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +88,6 @@ ROBOTSTXT_OBEY = False
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI= 'mongodb://localhost:27017'
+MONGO_DB= 'goodreads'
