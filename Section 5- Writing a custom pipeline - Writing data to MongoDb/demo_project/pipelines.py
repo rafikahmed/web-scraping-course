3e90b067f4c9106c7112d9bc1bed1b36d6536984
rafikahmed <djerahahmedrafik@mail.com>
2018-08-28 16:17:42 +0100

    Add MongoDbPipeline class

--- Section 5- Writing a custom pipeline - Writing data to MongoDb/demo_project/pipelines.py
+++ Section 5- Writing a custom pipeline - Writing data to MongoDb/demo_project/pipelines.py
@@ -6,6 +6,30 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class DemoProjectPipeline(object):
+from pymongo import MongoClient
+
+
+class MongoDbPipeline(object):
+    collection= 'Quotes'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri= mongo_uri
+        self.mongo_db= mongo_db
+    
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db= crawler.settings.get('MONGO_DB')
+        )
+    
+    def open_spider(self, spider):
+        self.client= MongoClient(self.mongo_uri)
+        self.db= self.client[self.mongo_db]
+    
+    def closed_spider(self, spider):
+        self.client.close()
+    
     def process_item(self, item, spider):
+        self.db[self.collection].insert_one(dict(item))
         return item
