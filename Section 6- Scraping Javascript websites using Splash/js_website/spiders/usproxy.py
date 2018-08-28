5a9a25f080e631e40eb4002c0480f00bdd57bc1f
rafikahmed <djerahahmedrafik@mail.com>
2018-08-28 16:34:59 +0100

    Splash Request

--- Section 6- Scraping Javascript websites using Splash/js_website/spiders/usproxy.py
+++ Section 6- Scraping Javascript websites using Splash/js_website/spiders/usproxy.py
@@ -7,7 +7,16 @@ class UsproxySpider(scrapy.Spider):
     allowed_domains= ['us-proxy.org']
     start_urls= ['http://us-proxy.org']
 
+
+    def start_requests(self):
+        url= 'https://us-proxy.org'
+        yield SplashRequest(url=url, callback=self.parse, endpoint='render.html', args={'wait': 0.5})
     
 
     def parse(self, response):
-        pass
+        for tr in response.xpath("//table[@id='proxylisttable']/tbody/tr"):
+            yield {
+                'ip': tr.xpath(".//td[1]/text()").extract_first(),
+                'port': tr.xpath(".//td[2]/text()").extract_first()
+            }
+    
