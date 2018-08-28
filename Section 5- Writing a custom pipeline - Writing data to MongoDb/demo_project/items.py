b2f8c1d536eea246ed87a9c42a255fa3802afc9b
rafikahmed <djerahahmedrafik@mail.com>
2018-08-28 16:15:53 +0100

    initial commit

---
+++ Section 5- Writing a custom pipeline - Writing data to MongoDb/demo_project/items.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
+from w3lib.html import remove_tags
+
+def remove_quotations(value):
+    return value.replace(u"\u201d", '').replace(u"\u201c", '')
+
+def strip_value(value):
+    return value.strip()
+
+
+class QuoteItem(scrapy.Item):
+    text= scrapy.Field(
+        input_processor= MapCompose(strip_value, remove_quotations),
+        output_processor= TakeFirst()
+    )
+    author= scrapy.Field(
+        input_processor= MapCompose(remove_tags),
+        output_processor= TakeFirst()
+    )
+    tags= scrapy.Field(
+        input_processor= MapCompose(remove_tags),
+        output_processor= Join(',')
+    )
---
+++ Section 5- Writing a custom pipeline - Writing data to MongoDb/demo_project/spiders/goodreads.py
@@ -0,0 +1,29 @@
+import scrapy
+from scrapy.loader import ItemLoader
+from demo_project.items import QuoteItem
+
+
+class GoodReadsSpider(scrapy.Spider):
+    #identity
+    name="goodreads"
+
+    start_urls= [
+        'https://www.goodreads.com/quotes?page=1'
+    ]
+
+    #Response
+    def parse(self, response):
+        for quote in response.xpath("//div[@class='quote']"):
+            loader= ItemLoader(item=QuoteItem(), selector=quote, response=response)
+            loader.add_xpath('text', ".//div[@class='quoteText']/text()[1]")
+            loader.add_xpath('author', ".//div[@class='quoteText']/child::a")
+            loader.add_xpath('tags', ".//div[@class='greyText smallText left']/a")
+            yield loader.load_item()
+            
+        
+        # /quotes?page=2
+        next_page= response.xpath("//a[@class='next_page']/@href").extract_first()
+
+        if next_page is not None:
+            next_page_link= response.urljoin(next_page)
+            yield scrapy.Request(url=next_page_link, callback=self.parse)
